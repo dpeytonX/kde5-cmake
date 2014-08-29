e54c09a3066cdca1a8bafd622e201422852a0435
Dametrious Peyton <dametrious.peyton@raritan.com>
2014-08-29 17:25:54 -0400

    master: initial import

---
+++ src/main/main.cpp
@@ -0,0 +1,24 @@
+#include <project.h>
+
+#include <KDeclarative/QmlObject>
+
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QtQml>
+#include <qglobal.h>
+#include <QString>
+
+Q_DECL_EXPORT int main(int argc, char* argv[]) {
+    QScopedPointer<QGuiApplication> app(new QGuiApplication ( argc, argv ));
+
+    app->setApplicationDisplayName(app->tr("Project"));
+    app->setApplicationVersion(PROJECT_VERSION_MAJOR + "." + PROJECT_VERSION_MINOR);
+
+    QQmlApplicationEngine* viewer = new QQmlApplicationEngine(app.data());
+
+    QUrl qurl( QStringLiteral ( "qrc:///qml/qt5/main.qml" ) );
+    KDeclarative::QmlObject* context = new KDeclarative::QmlObject(viewer, app.data());
+    context->setSource (qurl);
+
+    return app->exec();
+}
